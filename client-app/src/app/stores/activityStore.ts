2ad4d72cfb7c640a4ea7b09af2fbef49a1f05992
BZ <besartzeqiri54@gmail.com>
2022-04-21 16:48:57 +0200

    Instalimi i react router dom

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -26,18 +26,14 @@ export default class ActivityStore{
     loadActivities = async () => {
         try {
             const activities = await agent.Activities.list();
-
                 activities.forEach(activity =>{
-                    activity.date = activity.date.split('T') [0];
-                    this.activityRegistry.set(activity.id, activity);
+                activity.date = activity.date.split('T') [0];
+                this.activityRegistry.set(activity.id, activity);
             })
             this.setLoadingInitial(false);
-             
         } catch (error) {
             console.log(error);
                 this.setLoadingInitial(false);
-
-
         }
     }
     setLoadingInitial = (state: boolean) => {
